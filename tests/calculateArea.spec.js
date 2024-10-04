37d12aa82a5cf417a38ff07aa8cb8a7370092c27
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-10-04 18:47:50 +0200

    done

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -12,3 +12,27 @@ describe("Iteration 3", () => {
 })
 
 */
+
+describe("Iteration 3 | Calculate Area", () => {
+  describe("Function calculateArea", () => {
+    it("should be defined", () => {
+      expect(calculateArea).toBeDefined();
+    });
+
+    it("should take two arguments", () => {
+      expect(calculateArea.length).toBe(2);
+    });
+
+    it("should return a number representing the area of a rectangle", () => {
+      expect(divide(10, 2)).toEqual(20);
+      expect(divide(6, 3)).toEqual(18);
+      expect(divide(10, 5)).toEqual(50);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(calculateArea(1)).toEqual(undefined);
+      expect(calculateArea()).toEqual(undefined);
+      expect(calculateArea(undefined, 1)).toEqual(undefined);
+    });
+  });
+});
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -12,3 +12,26 @@ describe("Iteration 2", () => {
 })
 
 */
+describe("Iteration 2 | Divide", () => {
+  describe("Function divide", () => {
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
+
+    it("should take two arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+
+    it("should return the division of the two numbers", () => {
+      expect(divide(10, 2)).toEqual(5);
+      expect(divide(6, 3)).toEqual(2);
+      expect(divide(100, 50)).toEqual(2);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(divide(1)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+      expect(divide(undefined, 1)).toEqual(undefined);
+    });
+  });
+});
